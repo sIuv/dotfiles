f77262b54da10569605f0b0be2a210309b5f0867
Pontus Englund <pontus.englund@tobii.com>
2019-11-22 13:26:42 +0100

    Added first draft of emacs integration.

---
+++ emacs/emacs.d.symlink/init.el
@@ -0,0 +1,40 @@
+;;; package --- Main init file
+;;;
+;;; Commentary:
+;;;
+;;; Base was generated with http://www.emacs-bootstrap.com/.
+;;;
+;;;
+;;; Code:
+
+(add-to-list 'load-path (concat user-emacs-directory "elisp"))
+
+(require 'base)
+(require 'base-theme)
+(require 'base-extensions)
+(require 'base-functions)
+(require 'base-global-keys)
+
+(require 'lang-python)
+
+(require 'lang-ruby)
+
+(require 'lang-go)
+
+(require 'lang-php)
+
+(require 'lang-javascript)
+
+(require 'lang-web)
+
+(require 'lang-haskell)
+
+(require 'lang-elixir)
+
+(require 'lang-rust)
+
+(require 'lang-racket)
+
+(require 'lang-c)
+
+(require 'lang-org)
